3b8407096cd3893f327045168b7c80f2affe3b89
xu20161009 <xu20161009@gmail.com>
2017-11-03 17:26:04 +0800

    加入新接口，前端交互改为json

--- face_web.py
+++ face_web.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
+from urllib import parse
 
 
 FACE_SET_PATH = 'E:/faceimg/faceset/'
@@ -16,15 +17,28 @@ FACE_VERIFY_PATH = 'E:/faceimg/faceverify/'
 
 app = Flask(__name__)
 
+
 @app.route('/addFace.do', methods=['GET', 'POST'])
 def add_faceset():
     print(request.method)
+    result_mes = {'success': 1, 'data': {}, 'errorCode': 300, 'errorMsg': ''}
     result_add = {'status': 1}
+
     # print(request.form)
+
     if request.method == 'POST':
-        base64_str = str(request.form['data'])
-        username = str(request.form['id'])
-        base64_str = base64_str.replace(' ', '+')
+        tem_data = request.get_json(force=True)
+
+        username = str(tem_data['id'])
+        base64_str = str(tem_data['data'])
+
+        username = parse.unquote(username)
+        base64_str = parse.unquote(base64_str)
+
+        # base64_str = str(request.form['data'])
+        # username = str(request.form['id'])
+        # base64_str = base64_str.replace(' ', '+')
+
         imgData = base64.b64decode(base64_str)
         img_path = FACE_SET_PATH + username + str(random.randint(0, 500)) + '.jpg'
         face_img = open(img_path, 'wb')
@@ -44,7 +58,8 @@ def add_faceset():
         face_locations = face_recognition.face_locations(image)
         if len(face_locations) == 0:
             result_add['status'] = 0
-            return json.dumps(result_add)
+            result_mes['data'] = result_add
+            return json.dumps(result_mes)
         face_encoding = face_recognition.face_encodings(image)[0]
         face_encoding = list(face_encoding)
         face_encoding.append(username)
@@ -55,11 +70,13 @@ def add_faceset():
         result_add['status'] = 1
         return json.dumps(result_add)
 
+
 @app.route('/verifyFace.do', methods=['GET', 'POST'])
 def verify_face():
     print(request.method)
     face_feather = []
     user_label = []
+    result_mes = {'success': 1, 'data': {}, 'errorCode': 300, 'errorMsg': ''}
     result_verify = {'status': False, 'username': ''}
     csv_file = pd.read_csv('../result/faceCSV/face1.csv')
     row = csv_file.shape[0]
@@ -69,11 +86,14 @@ def verify_face():
         face_feather.append(np.asarray(temp[0:128]))
         user_label.append(''.join(temp[128:129]))
 
-
-
     if request.method == 'POST':
-        base64_str = str(request.form['data'])
-        base64_str = base64_str.replace(' ', '+')
+        tem_data = request.get_json(force=True)
+        base64_str = str(tem_data['data'])
+
+        # base64_str = str(request.form['data'])
+        # base64_str = base64_str.replace(' ', '+')
+        base64_str = parse.unquote(base64_str)
+
         imgData = base64.b64decode(base64_str)
         img_path = FACE_VERIFY_PATH + 'verify' + str(random.randint(0, 500)) + '.jpg'
         face_img = open(img_path, 'wb')
@@ -91,14 +111,40 @@ def verify_face():
         face_locations = face_recognition.face_locations(image)
         if len(face_locations) == 0:
             result_verify['status'] = False
-            return json.dumps(result_verify)
+            result_mes['data'] = result_verify
+            return json.dumps(result_mes)
         face_enconding = face_recognition.face_encodings(image)[0]
         face_distance = face_recognition.face_distance(face_feather, face_enconding)
         face_distance = list(face_distance)
-        result_verify['status'] = True
-        if min(face_distance) < 0.6:
+
+        print(user_label[face_distance.index(min(face_distance))], min(face_distance))
+        if min(face_distance) < 0.5:
+            result_verify['status'] = True
             result_verify['username'] = user_label[face_distance.index(min(face_distance))]
-        return json.dumps(result_verify)
+        result_mes['data'] = result_verify
+        return json.dumps(result_mes)
+
+
+@app.route('/queryUser.do', methods=['GET', 'POST'])
+def query_user():
+    user_label = []
+    result_mes = {'success': 1, 'data': {}, 'errorCode': 300, 'errorMsg': ''}
+    query_res = {'status': True}
+
+    tem_data = request.get_json(force=True)
+    username = str(tem_data['id'])
+    username = parse.unquote(username)
+    csv_file = pd.read_csv('../result/faceCSV/face1.csv')
+    row = csv_file.shape[0]
+    for i in range(row):
+        temp = list(csv_file.loc[i])
+        user_label.append(''.join(temp[128:129]))
+    if username in user_label:
+        query_res['status'] = False
+    else:
+        query_res['status'] = True
+    result_mes['data'] = query_res
+    return json.dumps(result_mes)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8585, debug=True)
